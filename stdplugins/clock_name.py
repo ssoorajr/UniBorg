61c63d9ea74b2eca2442be88fdf5e72c58d4af51
rsoorajs <rsoorajs@gmail.com>
2019-05-12 21:25:22 +0530

    Update clock_name.py

--- stdplugins/clock_name.py
+++ stdplugins/clock_name.py
@@ -19,7 +19,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"{HM}, {DMY}"
+        name = f"{HM}🌿Solo Traveler🌱📅{DMY}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
