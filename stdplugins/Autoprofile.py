181f5ee8f5e344507f3d11c6b6475a61747de9f1
rsoorajs <rsoorajs@gmail.com>
2019-05-21 18:52:50 +0530

    Update Autoprofile.py

--- stdplugins/Autoprofile.py
+++ stdplugins/Autoprofile.py
@@ -10,7 +10,7 @@ from telethon.errors import FloodWaitError
 from uniborg.util import admin_cmd
 
 
-DEL_TIME_OUT = 120
+DEL_TIME_OUT = 300
 
 
 @borg.on(admin_cmd("autobio"))  # pylint:disable=E0602
